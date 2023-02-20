9ec09cb08f86b2d12fb72df74d892b6cccfd7c7e
hiieeuu-ngo <102124558+hiieeuu-ngo@users.noreply.github.com>
2023-02-19 23:41:09 -0500

    Update Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -1,17 +1,40 @@
 public class Grid {
     public class Cell {
-        // TODO
+        private int value;
+        private Cell left;
+        private Cell right;
+        private Cell top;
+        private Cell bottom;
 
-        public Cell(){
-            // TODO
-            return;
+        public Cell(int value, Cell left, Cell right, Cell top, Cell bottom){
+            this.value = value;
+            this.left = left;
+            this.right = right;
+            this.top = top;
+            this.bottom = bottom;
         }
     }
     
-    // TODO
+    // Cell positions
+    /*
+     * 1 |  2 | 3  | 4
+     * 5 |  6 | 7  | 8
+     * 9 | 10 | 11 | 12
+     */
 
     public Grid(){
-        // TODO
+        Cell one = new Cell(25,null,10,null,1);
+        Cell two = new Cell(10,25,1,null,5);
+        Cell three = new Cell(1,10,10,null,5);
+        Cell four = new Cell(10,1,null,null,10);
+        Cell five = new Cell(1,null,5,25,1);
+        Cell six = new Cell(5,1,5,10,10);
+        Cell seven = new Cell(5,5,10,1,5);
+        Cell eight = new Cell(10,5,null,10,-1);
+        Cell nine = new Cell(1,null,10,1,null);
+        Cell ten = new Cell(10,1,1,5,null);
+        Cell eleven = new Cell(1,10,-1,5,null);
+        Cell twelve = new Cell(-1,1,null,10,null);
     }
 
     public boolean move(Cell box){
@@ -20,8 +43,10 @@ public class Grid {
     }
 
     public boolean check_complete(){
-        // TODO
-        return true;
+        if(one.value == nine.value || two.value == ten.value || three.value == eleven.value || four.value == twelve.value){
+            return true;
+        }
+        return false;
     }
 
     public void solve_game(){
