8a7cd7cfbd876422ed2cb777706f92dc5546cf63
hiieeuu-ngo <102124558+hiieeuu-ngo@users.noreply.github.com>
2023-02-19 23:37:12 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // Vous pouvez tester votre code ici!
+        //Node test = new Node(2);
+        //test.addValue(4);
+        //test.addValue(2);
+        //test.addValue(3);
+        //test.addValue(3);
+        //System.out.println(test.length_iteratif());
+        //test.removeValue(2);
+        //test.removeLast();  
+        //Node two = new Node(3);
+        //two.addValue(4);
+        //test.addNode(two);
+        //test.addValue_ordered(5);
+        //test.addValue_ordered(3);
+        //test.addValue_ordered(6);
+        //System.out.println(test.length_iteratif());
+        //System.out.println(test.length_recurssion());
+        //test.printList();
+        //test.insertSort();
+        //test.printList();
+        //System.out.println(test.returnNlast(2));
+        File test = new File();
+        test.push(1);
+        test.push(2);
+        test.push(4);
+        test.print();
+        test.remove(2);
+        test.print();
+        //System.out.println(test.search(4));
+        //System.out.println(test.pop());
+        //System.out.println(test.pop());
+        //System.out.println(test.pop());
     }
 }
